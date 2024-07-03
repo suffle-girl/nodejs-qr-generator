e9b66f3cd58a2e13f673bf3908b6a7a312d66168
Šárka Flídrová <sarka.flidrova@email.cz>
2024-07-03 10:51:00 +0200

    Code cleared.

--- index.js
+++ index.js
@@ -26,11 +26,4 @@ inquirer
       if (err) throw err;
       console.log("File has been created!");
     });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
   });
